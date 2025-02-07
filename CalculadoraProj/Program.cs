fe0d3cb8b4e18df35fd5aa1e961cc55f3f240eb8
Luiz Otávio <134003535+S-accarose@users.noreply.github.com>
2025-02-07 10:02:18 -0300

    projeto

---
+++ CalculadoraProj/Program.cs
@@ -0,0 +1,21 @@
+﻿using Biblio;
+class Program 
+{
+  public static void Main (string[] args) 
+  {
+    Console.Write("Digite o primeiro número: ");
+    int a = int.Parse(Console.ReadLine() ?? "0");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.Write("Digite o segundo número: ");
+    int b = int.Parse(Console.ReadLine() ?? "0");
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.Write("Aqui estão os resultados:\n");
+    Console.WriteLine($"Soma: {Calculadora.Somar(a, b)}");
+    Console.WriteLine($"Subtração: {Calculadora.Subtrair(a, b)}");
+    Console.WriteLine($"Multiplicação: {Calculadora.Multiplicar(a, b):F2}");
+    Console.WriteLine($"Divisão: {Calculadora.Dividir(a, b):F2}");
+    Console.WriteLine($"Potência: {Calculadora.Potencia(a, b):F2}");
+  }
+}
---
+++ CalculadoraProj/biblio.cs
@@ -0,0 +1,26 @@
+namespace Biblio
+{
+  public static class Calculadora
+  {
+    public static int Somar(int a, int b)
+    {
+      return a + b;
+    }
+    public static int Subtrair(int a, int b)
+    {
+      return a - b;
+    }
+    public static int Multiplicar(int a, int b)
+    {
+      return a * b;
+    }
+    public static int Dividir(int a, int b)
+    {
+      return a / b;
+    }
+    public static int Potencia(int a, int b)
+    {
+      return a ^ b;
+    }
+  }
+}
